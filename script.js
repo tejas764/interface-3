60f3dfed93bfee03093e03c66f922fb5ce7d25e3
NightFuryyyyy <thejus.thomas@bcah.christuniversity.in>
2025-07-09 22:23:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenuToggle = document.querySelector(".hamburger-menu-toggle");
+hamburgerMenuToggle.onclick = () => {
+    hamburgerMenu.classList.toggle("show");
+}
 
+// let currentIndex = 0;
+// const slider = document.getElementById('slider');
+// const totalSlides = slider.children.length;
 
-
-    let currentIndex = 0;
-    const slider = document.getElementById('slider');
-    const totalSlides = slider.children.length;
-
-    function slide(direction) {
-      currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
-      slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
+// function slide(direction) {
+//     currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+//     slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+// }
